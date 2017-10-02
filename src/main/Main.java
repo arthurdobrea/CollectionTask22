85f979bf0450eb22742ba8d81b1f402be2c9b51e
arthurdobrea <adamaa14@gmail.com>
2017-10-02 12:33:27 +0300

    adding new block

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,26 +7,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int capacity = 100;
-        Map<Integer,Integer> lruCache = new LruCache<>(3);
+        Map<Integer,String> lruCache = new LruCache<>(50);
         List<String> users = new ArrayList<>(capacity);
-
+        Random random = new Random();
 
 
         for(int i = 0 ; i < capacity; ++i){
             users.add("User " + i);
         }
 
+        /*
+        For more obvious result change maxCapacity in Map to 3 and uncomment this block.
 
-        lruCache.put(1,1);
-        lruCache.put(2,2);
-        lruCache.put(3,3);
-        lruCache.put(4,4);
-        lruCache.put(1,1);
+        lruCache.put(1,"1");
+        lruCache.put(2,"2");
+        lruCache.put(3,"3");
+        lruCache.put(4,"3");
+         */
 
 
-        for(Map.Entry<Integer,Integer> entry:lruCache.entrySet()){
+
+        for(int i = 0 ; i < capacity; ++i){
+            lruCache.put(i,users.get(random.nextInt(capacity)));
+        }
+
+        for(Map.Entry<Integer,String> entry:lruCache.entrySet()){
             System.out.println(entry.getKey() +  "   " + entry.getValue());
         }
-        }
     }
+}
 
